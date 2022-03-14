bd174e83a15393e544d4fbf25fe64bb5440de0aa
younghwanjoe <hhgman2@gmail.com>
2022-03-14 22:24:13 +0900

    add Content-Security-Policy header for utteranc

--- next.config.js
+++ next.config.js
@@ -5,13 +5,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
+  script-src 'self' https://utteranc.es 'unsafe-eval' 'unsafe-inline' utterance.es;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
   font-src 'self';
-  frame-src giscus.app
+  frame-src utteranc.es;
 `
 
 const securityHeaders = [
