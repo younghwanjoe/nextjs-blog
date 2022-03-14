8f493faaa0a9d9070b7e0d6d7d20cb355f5eabe3
younghwanjoe <hhgman2@gmail.com>
2022-03-14 21:44:51 +0900

    update utterances config

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -63,13 +63,13 @@ const siteMetadata = {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://utteranc.es/
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
-      issueTerm: '', // supported options: pathname, url, title
-      label: '', // label (optional): Comment 💬
+      issueTerm: 'pathname', // supported options: pathname, url, title
+      label: 'comment', // label (optional): Comment 💬
       // theme example: github-light, github-dark, preferred-color-scheme
       // github-dark-orange, icy-dark, dark-blue, photon-dark, boxy-light
-      theme: '',
+      theme: 'github-light',
       // theme when dark mode
-      darkTheme: '',
+      darkTheme: 'photon-dark',
     },
     disqusConfig: {
       // https://help.disqus.com/en/articles/1717111-what-s-a-shortname
